35624fd03136c59f026b333fd4a477a7194d02f3
MATT\Matthew <mpuent1@students.towson.edu>
2017-12-28 20:16:56 -0500

    Added changes to worntoys.py and login

--- WornToys.py
+++ WornToys.py
@@ -1,10 +1,26 @@
 from flask import Flask ,render_template, request, redirect, jsonify, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# this connects flask to connect to the database
+app.config.from_pyfile('config.cfg')
+
+# make a database object and register it to the application
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    user_id = db.Column(db.INTEGER, primary_key=True, nullable=False)
+    first_name = db.Column(db.String(55))
+    last_name = db.Column(db.String(55))
+    email = db.Column(db.VARCHAR(50))
+    password = db.Column(db.VARCHAR(50))
+    username = db.Column(db.VARCHAR(50))
+
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
 
 
@@ -13,10 +29,26 @@ def dashboard():
     return render_template("dashboard.html")
 
 
-@app.route('/register')
-def register():
-    return render_template("login.html")
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print("method is post, process the form. ")
+        form_data = request.form
+        uname = form_data['input_uname']
+        passw = form_data['input_password']
+        # select the user where the username and password are in the db
+        user = User.query.filter_by(username=uname, password=passw).first()
+        # if the user query is not none, meaning it exists..
+        if user is not None:
+            session['username'] = uname
+            return redirect("/dashboard")
+        else:
+            return render_template('login.html', sucessval="FALSE")
+    else:
+        return render_template('login.html')
 
 
 if __name__ == '__main__':
+    app.secret_key = 'IUERHIUEHRG98347%DE$'
     app.run(debug=True)
+
--- wt_sql.sql
+++ wt_sql.sql
@@ -1,48 +1,35 @@
-create table seller
-(
-	seller_id int auto_increment
-		primary key,
-	user_id int not null,
-	first_name varchar(55) null,
-	last_name varchar(55) null,
-	email varchar(50) null
-)
-;
+CREATE DATABASE IF NOT EXISTS `WornToys`;
+USE `WornToys`;
 
-create index user_id
-	on seller (user_id)
-;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(55) DEFAULT NULL,
+  `last_name` varchar(55) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `username` varchar(50),
+  PRIMARY KEY (`user_id`)
+);
 
-create table toy
-(
-	toy_id int auto_increment
-		primary key,
-	seller_id int not null,
-	name varchar(55) null,
-	list_date datetime null,
-	toy_image varchar(55) null,
-	constraint toy_ibfk_1
-		foreign key (seller_id) references seller (seller_id)
-)
-;
+DROP TABLE IF EXISTS `seller`;
+CREATE TABLE `seller` (
+  `seller_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(55) DEFAULT NULL,
+  `last_name` varchar(55) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`seller_id`),
+  FOREIGN KEY (`user_id`) REFERENCES user(`user_id`)
+);
 
-create index seller_id
-	on toy (seller_id)
-;
-
-create table user
-(
-	user_id int auto_increment
-		primary key,
-	first_name varchar(55) null,
-	last_name varchar(55) null,
-	email varchar(50) null,
-	password varchar(50) null
-)
-;
-
-alter table seller
-	add constraint seller_ibfk_1
-		foreign key (user_id) references user (user_id)
-;
-
+DROP TABLE IF EXISTS `toy`;
+CREATE TABLE `toy` (
+	`toy_id` int(11) NOT NULL AUTO_INCREMENT,
+	`seller_id` int(11) NOT NULL,
+	`name` varchar(55) DEFAULT NULL,
+	`list_date` datetime DEFAULT NULL,
+	`toy_image` varchar(55) DEFAULT NULL,
+	PRIMARY KEY (`toy_id`),
+	FOREIGN KEY(`seller_id`) REFERENCES seller(`seller_id`)
+);
