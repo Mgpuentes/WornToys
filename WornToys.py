d59844d98162b276e3a00cb77a2701eb675bae40
MATT\Matthew <mpuent1@students.towson.edu>
2018-01-03 14:08:57 -0500

    Added user register and modified app.py

--- WornToys.py
+++ WornToys.py
@@ -1,13 +1,26 @@
-from flask import Flask ,render_template, request, redirect, jsonify, session
+from flask import Flask, render_template, request, redirect, jsonify, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators, BooleanField
+from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
+                               Length, EqualTo)
 
 app = Flask(__name__)
+app.secret_key = 'IUERHIUEHRG98347%DE$'
 
 # this connects flask to connect to the database
 app.config.from_pyfile('config.cfg')
 
 # make a database object and register it to the application
 db = SQLAlchemy(app)
+
+
+class LoginForm(FlaskForm):
+    firstName = StringField('First Name', validators=[DataRequired()])
+    lastName = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 
 class User(db.Model):
@@ -26,7 +39,17 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template("dashboard.html")
+    if 'username' in session:
+        user = session['username']
+        print("user from session: {}".format(user))
+        # get the group that they're in
+        # first get the user
+        user_obj = User.query.filter_by(username=user).first()
+        print("Username from db query : {}".format(user_obj.first_name))
+
+    else:
+        user = "NOT_SET"
+    return render_template('dashboard.html', user=user_obj)
 
 
 @app.route('/login/', methods=['GET', 'POST'])
@@ -48,7 +71,21 @@ def login():
         return render_template('login.html')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = LoginForm()
+    if request.method == 'POST':
+        new_user = User(first_name=form.firstName.data, last_name=form.lastName.data,
+                        email=form.email.data, password=form.password.data, username=form.username.data)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Thanks for registering')
+        return redirect('/login/')
+    return render_template('register.html', form=form)
+
+
 if __name__ == '__main__':
-    app.secret_key = 'IUERHIUEHRG98347%DE$'
     app.run(debug=True)
 
