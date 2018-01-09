41a849e3e70e66682ae7638b58fdb13f6727f47d
MATT\Matthew <mpuent1@students.towson.edu>
2018-01-09 11:03:34 -0500

    Added listings page and route

--- WornToys.py
+++ WornToys.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, validators, BooleanField
+from wtforms import StringField, PasswordField, validators, BooleanField, DateField
 from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
                                Length, EqualTo, InputRequired)
 
@@ -23,6 +23,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired()])
 
 
+class ListingForm(FlaskForm):
+    name = StringField('Toy Name', validators=[InputRequired()])
+    list_date = DateField('Date', validators=[InputRequired()])
+
+
 class User(db.Model):
     user_id = db.Column(db.INTEGER, primary_key=True, nullable=False)
     first_name = db.Column(db.String(55))
@@ -32,12 +37,20 @@ class User(db.Model):
     username = db.Column(db.VARCHAR(50))
 
 
+class Toy(db.Model):
+    toy_id = db.Column(db.INTEGER, primary_key=True, nullable=False)
+    seller_id = db.Column(db.INTEGER)
+    name = db.Column(db.String(55))
+    list_date = db.Column(db.DateTime)
+    toy_image = db.Column(db.VARCHAR(55))
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/dashboard')
+@app.route('/dashboard/')
 def dashboard():
     if 'username' in session:
         user = session['username']
@@ -48,7 +61,8 @@ def dashboard():
         print("Username from db query : {}".format(user_obj.first_name))
 
     else:
-        user = "NOT_SET"
+        return render_template('index.html')
+
     return render_template('dashboard.html', user=user_obj)
 
 
@@ -83,7 +97,31 @@ def register():
 
         flash('Thanks for registering')
         return redirect('/login/')
+    flash('All fields required')
     return render_template('register.html', form=form)
+
+
+@app.route('/listing/')
+def listing():
+    if 'username' in session:
+        user = session['username']
+        print("user from session: {}".format(user))
+        # get the group that they're in
+        # first get the user
+        user_obj = User.query.filter_by(username=user).first()
+        print("Username from db query : {}".format(user_obj.first_name))
+
+    else:
+        return render_template('login.html')
+
+    return render_template('listing.html', user=user_obj)
+
+
+@app.route('/listing-form/')
+def listingform():
+    form = ListingForm()
+
+    return render_template('listing-form.html', form=form)
 
 
 if __name__ == '__main__':
